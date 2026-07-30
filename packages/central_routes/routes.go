e70b63a70356ccbbcbc1a91d9fcfbfc036ba6fac
JISHNU V <jishnuv@JISHNUs-MacBook-Air.local>
2026-07-30 16:12:47 +0530

    cores added PATCH

--- packages/central_routes/routes.go
+++ packages/central_routes/routes.go
@@ -28,7 +28,7 @@ func SetUp(app *fiber.App, db *gorm.DB, rdb *redis.Client, jwtSecret, razorpayKe
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:5173,http://localhost:5173",
-		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
 	}))
